14d9e66c3ed8a636f973aebe5f0502be8299384f
Thi Nguyen <nguyenmanhthi7111994@gmail.com>
2024-09-21 09:16:11 +0000

    Update

--- Player_Detection/yolo_inference.py
+++ Player_Detection/yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO 
 
-model = YOLO('Player_Detection\models\2nd_approach\model_1.pt')
+model = YOLO('Player_Detection/models/2nd_approach/model_1.pt')
 
-results = model.predict('input_videos/08fd33_4.mp4',save=True)
+results = model.predict('/content/Juve_1.mp4',save=True)
 print(results[0])
 print('=====================================')
 for box in results[0].boxes:
