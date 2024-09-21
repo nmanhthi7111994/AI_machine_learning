6b5123004a5250eb1a52249221bb84e993c8b55b
Thi Nguyen <nguyenmanhthi7111994@gmail.com>
2024-09-21 16:47:50 +0700

    update the main

---
+++ Player_Detection/main.py
@@ -0,0 +1,12 @@
+from uitls import read_video, save_video
+
+def main():
+    #read the video
+    video_frames = read_video('')
+
+    #Save video
+    save_video(video_frames, '')
+
+
+if __name__ == '__main__':
+    main()
