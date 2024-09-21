4fa5c71138ffe58598e811a95e85f8bbd48911f7
Thi Nguyen <nguyenmanhthi7111994@gmail.com>
2024-09-21 15:59:03 +0700

    commit the model

--- Player_Detection/yolo_inference.py
+++ Player_Detection/yolo_inference.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO 
 
-model = YOLO('models/best.pt')
+model = YOLO('Player_Detection\models\2nd_approach\model_1.pt')
 
 results = model.predict('input_videos/08fd33_4.mp4',save=True)
 print(results[0])
