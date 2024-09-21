7f2ac32d98d5d97706c1e831770fcd8bd40a66dc
Thi Nguyen <46949699+nmanhthi7111994@users.noreply.github.com>
2024-09-21 21:59:56 +0700

    Update main.py

--- Player_Detection/main.py
+++ Player_Detection/main.py
@@ -1,12 +1,18 @@
-from uitls import read_video, save_video
+from utils import read_video, save_video
+from tracker import Tracker
 
 def main():
-    #read the video
-    video_frames = read_video('')
+    # Read the video
+    video_frames = read_video('/content/Juve_1.mp4')
 
-    #Save video
-    save_video(video_frames, '')
+    #Intializer Tracker
+    tracker = Tracker('/content/AI_machine_learning/Player_Detection/models/2nd_approach/model_1.pt')
+    tracks = tracker.get_object_tracks(video_frames)
 
+    # Save video
+    save_video(video_frames, '/content/output/output_1.avi')
+
+   
 
 if __name__ == '__main__':
     main()
