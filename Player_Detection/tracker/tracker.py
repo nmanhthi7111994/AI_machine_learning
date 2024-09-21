5e92ec39104cf6dc4ba9705f0db5049f7daf08b4
Thi Nguyen <nguyenmanhthi7111994@gmail.com>
2024-09-21 22:30:33 +0700

    1

--- Player_Detection/tracker/tracker.py
+++ Player_Detection/tracker/tracker.py
@@ -35,10 +35,10 @@ class Tracker:
 
         # Convert to supervision format
         detection_supervision = sv.Detections.from_ultralytics(detection)  
-        
+
         # Convert GoalKeeper to player object
-            for object_ind , class_id in enumerate(detection_supervision.class_id):
-                if cls_names[class_id] == "goalkeeper":
-                    detection_supervision.class_id[object_ind] = cls_names_inv["player"]
+        for object_ind , class_id in enumerate(detection_supervision.class_id):
+            if cls_names[class_id] == "goalkeeper":
+                detection_supervision.class_id[object_ind] = cls_names_inv["player"]
 
         print(detection_supervision)
