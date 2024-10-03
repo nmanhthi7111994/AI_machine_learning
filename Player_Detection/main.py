7c0cac013b40b04330d75ca330d77a24ad674990
Thi Nguyen <nguyenmanhthi7111994@gmail.com>
2024-10-03 17:39:22 +0000

    Update 1

--- Player_Detection/main.py
+++ Player_Detection/main.py
@@ -1,16 +1,33 @@
 from utils import read_video, save_video
 from tracker import Tracker
 import cv2
+from team_assigner import TeamAssigner
 
 def main():
     # Read the video
-    video_frames = read_video('/content/Juev_2.mp4')
+    video_frames = read_video('/content/Juve_2.mp4')
 
     #Intializer Tracker
-    tracker = Tracker('/content/AI_machine_learning/Player_Detection/models/2nd_approach/model_1.pt')
+    tracker = Tracker('/content/AI_machine_learning/Player_Detection/models/2nd_approach/3th_player_detection_best.pt.pt')
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='/content/AI_machine_learning/Player_Detection/stubs/track_stubs_3.pkl')
+
+    # Interpolate Ball Positions
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+     # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
     #Save cropped image of a player
     for track_id, player in tracks['players'][0].items():
--- Player_Detection/tracker/tracker.py
+++ Player_Detection/tracker/tracker.py
@@ -28,6 +28,19 @@ class Tracker:
             detections += detections_batch
         return detections
 
+  def interpolate_ball_positions(self,ball_positions):
+        ball_positions = [x.get(1,{}).get('bbox',[]) for x in ball_positions]
+        df_ball_positions = pd.DataFrame(ball_positions,columns=['x1','y1','x2','y2'])
+
+        # Interpolate missing values
+        df_ball_positions = df_ball_positions.interpolate()
+        df_ball_positions = df_ball_positions.bfill()
+
+        ball_positions = [{1: {"bbox":x}} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
+
+
   def draw_ellipse(self,frame,bbox,color,track_id=None):
         y2 = int(bbox[3])
         x_center, _ = get_center_of_bbox(bbox)
